1e69390fbe9a4d2e89b91d0fb02af9bbda60eaea
uto8 <70425214+uto8@users.noreply.github.com>
2022-08-11 11:39:32 +0900

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "users#index"
+  get  "users/new"  => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
