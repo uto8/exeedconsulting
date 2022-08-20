c89445c563ec844c86a378eac554ef4dfc17d884
uto8 <70425214+uto8@users.noreply.github.com>
2022-08-20 19:00:33 +0900

    アクセス制限追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  def autheniticate_user
+    if current_user==nil
+      flash[:error]="ログインが必要です"
+      redirect_to("/users/sign_in")
+    end
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    unless current_user == user
+      flash[:error] = "アカウントが違います"
+      redirect_to("/")
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :autheniticate_user, {only: [:new, :create, :index]}
+  # before_action :order_user, {only: [:edit]}
+
   def new
     @item = Item.find(params[:id])
     @order = Order.new
@@ -20,9 +23,18 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  private
   def order_params
     params.require(:order).permit(
       :buyer_id, :seller_id, :item_id
     )
   end
+
+  def order_user
+    @orders = current_user.orders
+    @order = @orders.find_by(id: params[:id])
+    flash[:error]="アカウントが違います"
+    redirect_to root_path unless @order
+  end
+
 end
